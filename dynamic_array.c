2905b66ef9e6602758d01d3aef6e5b67d3d8cf05
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-10-23 20:58:55 -0400

    Implement initialization and basic add no resize to the source file

---
+++ dynamic_array.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dynamic_array.h"
+
+// Set capacity, initial length
+// Create instance of struct - allocate memory
+struct DynamicArray * _init() {
+
+    // Allocate memory for the struct DynamicArray
+    // Use sizeof to allocate just enough memory to hold the struct itself right now
+    struct DynamicArray *da = malloc(sizeof(struct DynamicArray));
+
+    // Initialize the structs fields (members)
+    da->length = 0;
+    da->capacity = 4;
+
+    // Allocate memory for the data the struct DyanmicArray will hold
+    // Each int typically takes sizeof(int) bytes
+    // Total allocated = sizeof(int) * capacity bytes (4 ints × 4 bytes each on most systems)
+    da->ptrData = malloc(sizeof(int) * da->capacity);
+
+    // Return the created struct DynamicArray
+    return da;
+}
+
+/// Adds an integer to the back of the DynamicArray
+int _add(struct DynamicArray *da, int element) { 
+
+    // Ensure capacity
+    if (da->length < da->capacity) {
+        da->ptrData[da->length++] = element;
+    }
+}
+
+
+int _get(int index) {}
+
+int _remove(int element) { }
+
+void _discard(struct DynamicArray) {}
+
+void _print(struct DynamicArray *da) {
+    for (int i = 0; i < da->length; i++) {
+        printf("[%d] %d\n", i, da->ptrData[i]);
+    }
+}
