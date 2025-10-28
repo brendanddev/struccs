e0e92a5cb81d6e4202867e124932057dba3d881a
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-10-28 18:34:54 -0400

    Setup source for ga

---
+++ generic_array/generic_array.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "generic_array.h"
+
+
+struct GenericArray * _init() { }
+void _add(struct GenericArray *ga) { }
+void _get(struct GenericArray *ga) { }
+void _remove(struct GenericArray *ga) { }
+void _discard(struct GenericArray *ga) { }
