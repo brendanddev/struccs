e1cf5921202efd24eff45934868853f98b13f7f8
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-10-29 17:41:21 -0400

    Update main

--- dynamic_array/main.c
+++ dynamic_array/main.c
@@ -5,9 +5,21 @@
 
 /// main.c
 
+
 int main() {
 
     struct DynamicArray *da = _init();
+
+    // This will currently produce an error: zsh: segmentation fault  ./out
+    // This is due to the fact that the DynamicArray currently does not resize
+    // And its initial capacity is only 4. So only space is allocated for 4 ints
+
+    // This causes the segmentation fault because the pointer will try to access
+    // memory that may be in use or will be used by another system
+    for (int i = 0; i < 50; i++) {
+        int rd_num = rand() % 100;
+        da->ptrData[i] = rd_num;
+    }
 
     _status(da);
     _add(da, 10);
