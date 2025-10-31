8293ab3af4d7af3d8a7a89559a79637b3c95b21f
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-10-31 09:08:53 -0400

    Update main

--- dynamic_array/main.c
+++ dynamic_array/main.c
@@ -8,32 +8,36 @@
 
 int main() {
 
+    // Create an instance of the DynamicArray
     struct DynamicArray *da = _init();
 
-    // This will currently produce an error: zsh: segmentation fault  ./out
-    // This is due to the fact that the DynamicArray currently does not resize
-    // And its initial capacity is only 4. So only space is allocated for 4 ints
+    // Print current status and contents of the arr
+    _print(da);
 
-    // This causes the segmentation fault because the pointer will try to access
-    // memory that may be in use or will be used by another system
+
+    // Append 50 random ints to the arr
     for (int i = 0; i < 50; i++) {
         int rd_num = rand() % 101;
         _append(da, rd_num);
-        // da->ptrData[i] = rd_num;
-        // or 'da->ptrData[da->length++] = item;' with of conditional in the _add func
     }
 
+    // Print current status and contents after appending 50 ints
     _print(da);
-    _status(da);
 
-    // Get item at index 0
-    // int val = _get(da, 0);
-    // printf("GET [0]: %d\n", val);
 
-    // Remove last item
-    // _remove_last(da);
-    // _print(da);
-    // _status(da);
+    // Get the item at index 0
+    int value = _get(da, 0);
+    printf("\nValue at index 0: %d\n", value);
+
+
+    // Remove the last item in the arr
+    printf("Removing the last item...\n");
+    _remove_last(da);
+    _print(da);
+
+
+
+
 
     // printf("BEFORE REMOVE AT: \n");
     // _print(da);
@@ -44,10 +48,34 @@ int main() {
     // _print(da);
     // _status(da);
 
-    printf("ADD AT INDEX: 1\n");
-    _add(da, 999, 1);
-    _print(da);
-    _status(da);
+    // printf("ADD AT INDEX: 1\n");
+    // _add(da, 999, 1);
+    // _print(da);
+    // _status(da);
+
+
+
+
+
+
+
+
+
+
+    // This will currently produce an error: zsh: segmentation fault  ./out
+    // This is due to the fact that the DynamicArray currently does not resize
+    // And its initial capacity is only 4. So only space is allocated for 4 ints
+
+    // This causes the segmentation fault because the pointer will try to access
+    // memory that may be in use or will be used by another system
+    // for (int i = 0; i < 50; i++) {
+    //     int rd_num = rand() % 101;
+    //     _append(da, rd_num);
+    //     // da->ptrData[i] = rd_num;
+    //     // or 'da->ptrData[da->length++] = item;' with of conditional in the _add func
+    // }
+
+
 
 
 
