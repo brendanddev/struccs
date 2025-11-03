9bff1dbe361050c3ce3d659c05573a10f89c8dc7
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-11-03 17:20:03 -0500

    Update main to test

--- generic_array/main.c
+++ generic_array/main.c
@@ -5,12 +5,38 @@
 
 
 /// main.c
+/// Brendan Dileo - November 3 2025
+
+void _print(struct GenericArray *ga);
 
 int main() {
 
     struct GenericArray *ga = _init(sizeof(int));
     printf("Capacity: %d, Length: %d, Item Size: %d\n", ga->capacity, ga->length, ga->item_size);
 
+    int num = 100;
+    void *ptr = &num;
+    _append(ga, ptr);
+    printf("Capacity: %d, Length: %d, Item Size: %d\n", ga->capacity, ga->length, ga->item_size);
 
+    int num2 = 200;
+    void *ptr2 = &num2;
+    _append(ga, ptr2);
+    
+    int num3 = 300;
+    void *ptr3 = &num3;
+    _append(ga, ptr3);
+
+    printf("Capacity: %d, Length: %d, Item Size: %d\n", ga->capacity, ga->length, ga->item_size);
+
+    _print(ga);
     return 0;
+}
+
+
+
+void _print(struct GenericArray *ga) {
+    for (int i = 0; i < ga->length; i++) {
+        printf("[%d] - %d\n", i, *(int *)((char *)ga->ptrData + i * ga->item_size));
+    }
 }
