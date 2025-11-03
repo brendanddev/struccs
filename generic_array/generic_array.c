af4c8f0b45e61ec6c38bb581c6baca0d35b97b40
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-11-03 16:48:53 -0500

    Implement init func for GenericArray

---
+++ generic_array/generic_array.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "generic_array.h"
+
+
+/// generic_array.c
+
+struct GenericArray * _init(size_t item_size) {
+
+    // Allocate memory for the struct
+    struct GenericArray *ga = NULL;
+    ga = malloc(sizeof(struct GenericArray));
+
+    // Handle allocation failure
+    if (ga == NULL) {
+        fprintf(stderr, "Memory allocation failed during initialization.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    ga->capacity = 4;
+    ga->length = 0;
+    ga->item_size = item_size;
+
+    // Allocate memory for the internal array
+    int *pTmp = malloc(sizeof(item_size) * ga->capacity);
+    if (pTmp == NULL) {
+        printf(stderr, "Memory allocation failed for internal array.\n");
+        free(ga);               // Free previously allocated struct to avoid memory leak
+        exit(EXIT_FAILURE);
+    }
+
+    ga->ptrData = pTmp;
+    return ga;
+}
+
+
+
+bool _append(struct GenericArray *ga, void *itemPtr) { }
