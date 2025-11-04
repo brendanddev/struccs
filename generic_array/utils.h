b7f4055953ed3fa3c75697b4c51935127b76ecb3
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-11-04 15:51:15 -0500

    Update utils header

--- generic_array/utils.h
+++ generic_array/utils.h
@@ -1,7 +1,17 @@
+
+
+/// utils.h
+/// Brendan Dileo - November 4 2025
+
+#ifndef UTILS_H
+#define UTILS_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
 
-/// utils.h
-/// Brendan Dileo - November 4 2025
+struct GenericArray * create_array();
+void print_arr_int(struct GenericArray *ga);
+
+#endif
