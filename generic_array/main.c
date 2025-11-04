3178c9add3a5eb342ee2472d13272fc5e283e1b8
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-11-03 19:30:07 -0500

    Update main to test append, get, and printing

--- generic_array/main.c
+++ generic_array/main.c
@@ -7,17 +7,53 @@
 /// main.c
 /// Brendan Dileo - November 3 2025
 
-void _print(struct GenericArray *ga);
+// Prototypes
+void test_append();
+void test_get();
+void print_arr(struct GenericArray *ga);
+
 
 int main() {
+    test_append();
+    test_get();
+    return 0;
+}
 
+
+void test_append() {
+    printf("=========================================\n");
     struct GenericArray *ga = _init(sizeof(int));
-    printf("Capacity: %d, Length: %d, Item Size: %zu bytes\n", ga->capacity, ga->length, ga->item_size);
+    _print(ga);
 
     int num = 100;
     void *ptr = &num;
     _append(ga, ptr);
-    printf("Capacity: %d, Length: %d, Item Size: %zu bytes\n", ga->capacity, ga->length, ga->item_size);
+    _print(ga);
+
+    int num2 = 200;
+    void *ptr2 = &num2;
+    _append(ga, ptr2);
+    
+    int num3 = 300;
+    void *ptr3 = &num3;
+    _append(ga, ptr3);
+    _print(ga);
+
+    print_arr(ga);
+    _discard(ga);
+    ga = NULL;
+    printf("=========================================\n");
+
+}
+
+void test_get() {
+    printf("=========================================\n");
+    struct GenericArray *ga = _init(sizeof(int));
+    _print(ga);
+
+    int num = 100;
+    void *ptr = &num;
+    _append(ga, ptr);
 
     int num2 = 200;
     void *ptr2 = &num2;
@@ -27,18 +63,27 @@ int main() {
     void *ptr3 = &num3;
     _append(ga, ptr3);
 
-    printf("Capacity: %d, Length: %d, Item Size: %zu bytes\n", ga->capacity, ga->length, ga->item_size);
+    void *value_ptr;
+    if (_get(ga, 1, &value_ptr)) {
+        printf("GET VALUE: %d\n", (int *)value_ptr);
+    } else {
+        printf("GET operation failed.\n");
+    }
 
-    _print(ga);
+    void *value_ptr2;
+    if (_get(ga, 0, &value_ptr2)) {
+        printf("GET VALUE: %d\n", (int *)value_ptr2);
+    } else {
+        printf("GET operation failed.\n");
+    }
 
-    _discard(ga);
+    free(ga);
     ga = NULL;
-    return 0;
 }
 
 
 
-void _print(struct GenericArray *ga) {
+void print_arr(struct GenericArray *ga) {
     for (int i = 0; i < ga->length; i++) {
         printf("[%d] - %d\n", i, *(int *)((char *)ga->ptrData + i * ga->item_size));
     }
