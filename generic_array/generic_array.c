e74c9aba7c95c9a320e998609a59aa3237af5ca1
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-11-03 20:45:19 -0500

    Implement resize func logic

--- generic_array/generic_array.c
+++ generic_array/generic_array.c
@@ -11,6 +11,7 @@
 
 // Prototypes
 static void shift_right(struct GenericArray *ga, int index);
+static void resize(struct GenericArray *ga);
 
 
 struct GenericArray * _init(size_t item_size) {
@@ -43,6 +44,12 @@ struct GenericArray * _init(size_t item_size) {
 
 // Adds an item to the end of the array
 bool _append(struct GenericArray *ga, void *itemPtr) { 
+
+    // Check if resize is needed
+    if (ga->length == ga->capacity) {
+        resize(ga);
+    }
+
     // Calculate the address where the new item should be stored
     // Starting at the base of the internal array (ga->ptrData)
     // Move forward by (length * item_size) bytes to reach the next free slot
@@ -60,9 +67,19 @@ bool _append(struct GenericArray *ga, void *itemPtr) {
 bool _add(struct GenericArray *ga, int index, void *in_ptr) {
     if (index < 0 || index > ga->length) return false;
 
+    // Check if resize is needed
+    if (ga->length == ga->capacity) {
+        resize(ga);
+    }
+
+    // Shift subsequent items to the right to make room for item being inserted
     shift_right(ga, index);
+
+    // Calculate the insertion point
     void *dest_ptr = (char *) ga->ptrData + index * ga->item_size;
-    dest_ptr = in_ptr;
+
+    // Copy the memory of the item into destination and increment length
+    memcpy(dest_ptr, in_ptr, ga->item_size);
     ga->length++;
     return true;
 }
@@ -98,13 +115,33 @@ void _print(struct GenericArray *ga) {
 // Some source, a destination, and memcpy to get it between
 static void shift_right(struct GenericArray *ga, int index) {
     // Loop from end to insertion point
-    for (int i = ga->length; i > index; i--) {
+    for (int i = ga->length - 1; i >= index; i--) {
         // Calculate the memory address of the source item at index i and the destination item at index i + 1
         void *src = (char *) ga->ptrData + i * ga->item_size;
         void *dest = (char *) ga->ptrData + (i + 1) * ga->item_size;
 
         // Copy the memory from src* into dest* effectively doing i + 1 = i
         memcpy(dest, src, ga->item_size);
+        // memmove(dest, src, ga->item_size);
+    }
+}
+
+// Resizes the internal array when its limit is reached
+static void resize(struct GenericArray *ga) {
+    // Calculate new capacity
+    int new_capacity = ga->capacity * 2;
+
+    // Reallocate more memory for the new array size
+    void *tmp_ptr = NULL;
+    tmp_ptr = realloc(ga->ptrData, new_capacity * ga->item_size);
+
+    // Handle reallocation failure
+    if (tmp_ptr == NULL) {
+        fprintf(stderr, "Memory allocation failed during shrinking\n");
+        exit(EXIT_FAILURE);
     }
 
+    // Set new capacity and reassign pointer to memory
+    ga->capacity = new_capacity;
+    ga->ptrData = tmp_ptr;
 }
