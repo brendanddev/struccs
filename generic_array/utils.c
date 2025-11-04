8d17ba27bae73034df422a1113c08b7df6b0ea07
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-11-04 15:45:10 -0500

    Create utility header file and source file

---
+++ generic_array/utils.c
@@ -0,0 +1,10 @@
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+/// utils.c
+/// Brendan Dileo - November 4 2025
---
+++ generic_array/utils.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+/// utils.h
+/// Brendan Dileo - November 4 2025
