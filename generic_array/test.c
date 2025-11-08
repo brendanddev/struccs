35a3f07f54767c4becf0d591299cee03d203ea93
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-11-08 12:24:53 -0500

    Update test source file to plan out logic

--- generic_array/test.c
+++ generic_array/test.c
@@ -12,6 +12,26 @@ int main() {
 
     // Reverse Function - Reverse elements in-place
 
+    int n = 4;
+    int arr[4] = { 100, 63, 11, 99 };
+
+    // In order
+    for (int i = 0; i < n; i++) printf("%d ", arr[i]);
+    printf("\n");
+
+    // Reverse order
+    for (int i = n - 1; i >= 0; i--) printf("%d ", arr[i]);
+
+    int n_rev = 4;
+    int arr_rev[4];
+    int idx = 0;
+    for (int i = n_rev - 1; i >= 0; i--) {
+        arr_rev[i] = arr[idx++];
+    }
+
+    printf("\nREVERSE ARRAY: \n");
+    for (int i = 0; i < n_rev; i++) printf("%d ", arr_rev[i]);
+
 
 
     return 0;
