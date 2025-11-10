d43a642cee2392df44eb004f41cf17ceae047aa8
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-11-10 16:22:51 -0500

    Create testing helpers header

---
+++ tests/test_helpers.h
@@ -0,0 +1,14 @@
+
+
+/// test_helpers.h
+/// Brendan Dileo - November 4 2025
+
+#ifndef TEST_HELPERS_H
+#define TEST_HELPERS_H
+
+#include "../generic_array/generic_array.h"
+#include <stdbool.h>
+
+
+
+#endif
