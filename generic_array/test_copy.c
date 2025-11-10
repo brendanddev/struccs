9723f57c63dfa33b3a3f6e507d5385bd480c5aad
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-11-10 11:04:17 -0500

    Update test copy

--- generic_array/test_copy.c
+++ generic_array/test_copy.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include "generic_array.h"
+
 
 /// test_copy.c
 /// Source code for testing/developing the copy logic for the GenericArray
@@ -9,31 +11,70 @@
 
 
 
-// - **Copy Function** - Deep/shallow copy of arrays
-//   - Copy array structure and all elements
-//   - Consider deep copy with custom copy function for complex types
+// Copy Function: Deep copy of GenericArrays
+// For a deep copy, memory needs to be allocated for the copy of the struct itself, but also memory for the data it points to
 
 // Ideas: 
 // memcpy --> Copy the required number of bytes from the initial memory location to the new memory location?
 int main() {
 
-    // Initial array to be copied
-    int n = 4;
-    int arr[4] = { 100, 999, 56, 22 };      // allocated on stack
-
+    // Initialize a GenericArray to hold integers, populate, and print initial state
+    int n = 5;
+    struct GenericArray *ga = _init(sizeof(int));
+    for (int i = 0; i < n; i++) _append(ga, &i);
     printf("Initial Array: \n");
-    for (int i = 0; i < n; i++) printf("%d ", arr[i]);
+    for (int i = 0; i < n; i++) printf("[%d] ", *(int *)((char *)ga->ptrData + i * ga->item_size));
     printf("\n");
 
-    // Allocate new memory for the new array
-    int *new_arr = NULL;
-    new_arr = malloc(sizeof(int) * n);      // dynamically allocated on heap
+    // Allocate new memory for the copy of the array
+    void *new_ga_ptrData = NULL;
+    new_ga_ptrData = malloc(ga->item_size * ga->capacity);
 
-    // Copy raw memory from the initial array to the copy of the array
-    memcpy(new_arr, arr, sizeof(int) * n);
+    // Allocate new memory for the copy of the struct (GenericArray)
+    struct GenericArray *new_ga = NULL;
+    new_ga = malloc(sizeof(struct GenericArray));
 
-    printf("Copied Array Contents: \n");
-    for (int i = 0; i < n; i++) printf("%d ", new_arr[i]);
+    // Copy raw memory from the original struct to the copy
+    memcpy(new_ga, ga, sizeof(struct GenericArray));
+
+    // Copy raw memory from the original array pointed to, to the copy
+    memcpy(new_ga_ptrData, ga->ptrData, ga->item_size * ga->length);
+
+    printf("Copied Array: \n");
+    for (int i = 0; i < n; i++) printf("[%d] ", *(int *)((char *) new_ga->ptrData + i * ga->item_size));
+    _reverse(new_ga);
+    printf("\n");
+    for (int i = 0; i < n; i++) printf("[%d] ", *(int *)((char *) new_ga->ptrData + i * ga->item_size));
+
+
+
+
+
+
+    // Free allocated memory
+    _discard(ga);
+    ga = NULL;
+    free(new_ga);
+    new_ga = NULL;
+
+
+    // // Initial array to be copied
+    // int n = 4;
+    // int arr[4] = { 100, 999, 56, 22 };      // allocated on stack
+
+    // printf("Initial Array: \n");
+    // for (int i = 0; i < n; i++) printf("%d ", arr[i]);
+    // printf("\n");
+
+    // // Allocate new memory for the new array
+    // int *new_arr = NULL;
+    // new_arr = malloc(sizeof(int) * n);      // dynamically allocated on heap
+
+    // // Copy raw memory from the initial array to the copy of the array
+    // memcpy(new_arr, arr, sizeof(int) * n);
+
+    // printf("Copied Array Contents: \n");
+    // for (int i = 0; i < n; i++) printf("%d ", new_arr[i]);
 
 
 
