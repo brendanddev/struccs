5bf1a493e796213414e68ff58b352e5a2ec4cae3
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-11-10 16:26:27 -0500

    Move logic from utils to test helpers

--- tests/test_helpers.h
+++ tests/test_helpers.h
@@ -9,6 +9,13 @@
 #include "../generic_array/generic_array.h"
 #include <stdbool.h>
 
-
+struct GenericArray * create_int_array();
+struct GenericArray * create_int_array_from_size(int size);
+struct GenericArray * create_dbl_array();
+struct GenericArray * create_chr_array();
+bool contains_int(void *a, void *b);
+void print_arr_int(struct GenericArray *ga);
+void print_arr_dbl(struct GenericArray *ga);
+void print_arr_chr(struct GenericArray *ga);
 
 #endif
