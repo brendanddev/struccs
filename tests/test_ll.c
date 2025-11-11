20968e200e841e399d4283f4df7e300f8225c18f
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-11-11 09:44:06 -0500

    Update tests

--- tests/test_ll.c
+++ tests/test_ll.c
@@ -8,8 +8,24 @@
 /// Testing and developmental file for the LinkedList implementation
 /// Brendan Dileo - November 11 2025
 
+// Address: 0x0 == NULL
+
 
 int main() {
+
+    // Create a new Node and LinkedList
+    struct Node *node = _node_init(10);
+    struct Node *node2 = _node_init(100);
+    struct LinkedList *linked_list = _init();
+
+    printf("Node Value: %d, Next Pointer: %p\n", node->value, node->next);
+    printf("LinkedList Length: %d, Head Pointer: %p\n", linked_list->length, linked_list->head);
+
+    _insert(node, linked_list);
+    _insert(node2, linked_list);
+
+    _print(linked_list);
+
 
 
 
