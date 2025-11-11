4facb11fb91e949a9f349ecfcff96f21bfd5500f
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-11-11 12:02:05 -0500

    Test tail insertion to try and fix bug with infinite loop

--- tests/test_ll.c
+++ tests/test_ll.c
@@ -16,15 +16,21 @@ int main() {
     // Create a new Node and LinkedList
     struct Node *node = _node_init(10);
     struct Node *node2 = _node_init(100);
+    struct Node *node3 = _node_init(999);
     struct LinkedList *linked_list = _init();
 
     printf("Node Value: %d, Next Pointer: %p\n", node->value, node->next);
     printf("LinkedList Length: %d, Head Pointer: %p\n", linked_list->length, linked_list->head);
 
+    printf("Head Insertion...\n");
     _insert(node, linked_list);
     _insert(node2, linked_list);
-
     _print(linked_list);
+
+    printf("Tail Insertion...\n");
+    _insert_tail(node3, linked_list);
+    _print(linked_list);
+
 
 
 
