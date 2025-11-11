532f1b94cd578b4d157283ada2e2901e799debfd
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-11-11 09:23:26 -0500

    Create test file for LinkedList

---
+++ tests/test_ll.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "../linked_list/linked_list.h"
+
+
+/// test_ll.c
+/// Testing and developmental file for the LinkedList implementation
+/// Brendan Dileo - November 11 2025
+
+
+int main() {
+
+
+
+    return 0;
+
+}
