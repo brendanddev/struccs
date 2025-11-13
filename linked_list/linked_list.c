311084e4731b004eb8ca8536690f3af32c3eb07f
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-11-13 18:51:35 -0500

    Fix logical issue with how the value is being copied into memory

--- linked_list/linked_list.c
+++ linked_list/linked_list.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 #include "linked_list.h"
 
 
@@ -20,20 +21,54 @@ struct Node * _node_init(void *val, size_t size) {
     // Handle allocation failure
     if (node == NULL) { }
 
-    // Point the value pointer to the value passed and set the item size
-    node->value = val;
+    // Set size of item stored in the node and initialize next and prev pointers to NULL
     node->item_size = size;
-
-    // Initially the Node will not point to any other nodes
     node->next = NULL;
     node->prev = NULL;
+
+    // Allocate memory for the type of data this node will store
+    void *tmp = NULL;
+    tmp = malloc(size);
+
+    // Handle allocation failure
+    if (tmp == NULL) { }
+
+    // Point the `value` pointer to the memory allocated for the value and
+    // copy the raw memory contents from the memory pointed to by `val` into `node->value`
+    node->value = tmp;
+    memcpy(node->value, val, node->item_size);
 
     return node;
 }
 
-
+// Initializes a new LinkedList
 struct LinkedList * _init() {
 
     struct LinkedList *list = NULL;
+    list = malloc(sizeof(struct LinkedList));
 
+    if (list == NULL) { }
+
+    list->length = 0;
+    list->head = NULL;
+    list->tail = NULL;
+    return list;
+}
+
+
+// Free the memory allocated by the Node
+void _node_discard(struct Node *node) { 
+    if (node != NULL) {
+        free(node->next);
+        free(node->prev);
+        free(node->value);
+        free(node);
+    }
+}
+
+// Free the memory allocated by the LinkedList
+void _discard(struct LinkedList *linked_list) { 
+    if (linked_list != NULL) {
+        free(linked_list);
+    }
 }
