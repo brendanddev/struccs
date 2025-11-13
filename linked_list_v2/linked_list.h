bb8b43585ccb2040b4558ada914c5f654e4b9b73
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-11-12 19:36:11 -0500

    Update header

---
+++ linked_list_v2/linked_list.h
@@ -0,0 +1,13 @@
+
+/// linked_list.h
+//// Header for the generic LinkedList implementation
+/// Brendan Dileo - November 11 2025
+
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#endif
