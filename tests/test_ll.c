7e12953c13d0da1ef24b3e4d1801a22fb6d28b3c
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-11-14 09:06:03 -0500

    Update tests

--- tests/test_ll.c
+++ tests/test_ll.c
@@ -15,38 +15,47 @@ void print_int_linked_list(struct LinkedList *list);
 
 int main() {
 
-    // Create a new Node and LinkedList
+    // Create a new LinkedList
     struct LinkedList *linked_list = _init();
+    printf("LinkedList - Length: %d, Head: %p, Tail: %p\n", linked_list->length, linked_list->head, linked_list->tail);
 
-
+    // Create a new Node
     int val = 100;
     void *ptr = &val;
     struct Node *node = _init_node(ptr, sizeof(int));
-    printf("Value: %d, Item Size: %zu, Next: %p, Previous: %p\n", * (int *) node->value, node->item_size, node->next, node->prev);
+    printf("Node - Item Size: %zu, Next: %p\n", node->item_size, node->next);
 
+    
+    // Insert at head
     _insert(node, linked_list);
-    printf("Inserted at head...\n");
-    printf("Value: %d, Item Size: %zu, Next: %p, Previous: %p\n", * (int *) node->value, node->item_size, node->next, node->prev);
     print_int_linked_list(linked_list);
 
-    int val2 = 999;
+    int val2 = 450;
     void *ptr2 = &val2;
     struct Node *node2 = _init_node(ptr2, sizeof(int));
     _insert(node2, linked_list);
-    printf("Inserted at head...\n");
-    printf("Value: %d, Item Size: %zu, Next: %p, Previous: %p\n", * (int *) node2->value, node2->item_size, node2->next, node2->prev);
     print_int_linked_list(linked_list);
 
-    int val3 = 9021;
+    int val3 = 9734;
     void *ptr3 = &val3;
     struct Node *node3 = _init_node(ptr3, sizeof(int));
     _insert(node3, linked_list);
-    printf("Inserted at head...\n");
-    printf("Value: %d, Item Size: %zu, Next: %p, Previous: %p\n", * (int *) node3->value, node3->item_size, node3->next, node3->prev);
     print_int_linked_list(linked_list);
 
+
     _discard_node(node);
+    node = NULL;
+    _discard_node(node2);
+    node2 = NULL;
+    _discard_node(node3);
+    node3 = NULL;
+
     _discard(linked_list);
+    linked_list = NULL;
+
+
+
+
 
     return 0;
 }
