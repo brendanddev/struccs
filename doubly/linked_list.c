2f7702a299c7dc55629c21148968ba994279c377
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-11-14 18:08:14 -0500

    Setup the doubly linked list structs and source

--- doubly/linked_list.c
+++ doubly/linked_list.c
@@ -11,3 +11,26 @@
 
 
 
+
+// // Definess the doubly Node struct type
+// typedef struct Node {
+//     void *value;                        // Pointer to the value stored in this node
+//     size_t item_size;                   // The size of the item stored in the node
+//     struct Node *next;                  // Pointer to the next node in the list
+//     struct Node *prev;                  // Pointer to the previous node in the list
+// } Node;
+
+// // Defines the LinkedList struct type
+// typedef struct LinkedList {
+//     int length;
+//     struct Node *head;                  // Pointer to the head node (first element)
+//     struct Node *tail;                  // Pointer to the tail node (last element)
+// } LinkedList;
+
+struct Node * ll_init_node(void *val, size_t size) {
+    struct Node *node = 
+}
+
+
+struct LinkedList * ll_init() {
+}
