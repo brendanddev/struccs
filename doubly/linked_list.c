fac993e487d233b0f9c78c8d2c1648976b0ca185
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-11-14 17:58:48 -0500

    Create doubly generic linked list header file and source file

---
+++ doubly/linked_list.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include "linked_list.h"
+
+
+/// linked_list.c
+/// A generic Doubly Linked List implementation
+/// Brendan Dileo - November 14 2025
+
+
+
---
+++ doubly/linked_list.h
@@ -0,0 +1,7 @@
+
+/// linked_list.h
+/// Header for the generic Doubly LinkedList implementation
+/// Brendan Dileo - November 14 2025
+
+
+
