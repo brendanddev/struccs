50b10f6c252929700836d7201dc9906e64d33d50
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-11-17 11:59:28 -0500

    Setup next phase of func implementations for the Linked List

--- linked_list/linked_list.h
+++ linked_list/linked_list.h
@@ -28,14 +28,24 @@ typedef struct LinkedList {
 } LinkedList;
 
 
-struct Node * ll_init_node(void *val, size_t size);
-struct LinkedList * ll_init();
+struct Node* ll_init_node(void *val, size_t size);
+struct LinkedList* ll_init();
 void ll_insert(struct Node *node, struct LinkedList *list);
 void ll_insert_tail(struct Node *node, struct LinkedList *list);
 void ll_insert_at(struct Node *node, struct LinkedList *list, int index);
 void ll_remove(struct LinkedList *list);
 void ll_remove_tail(struct LinkedList *list);
 void ll_remove_at(struct LinkedList *list, int index);
+
+bool ll_get(struct LinkedList *list, int index, void *out);
+bool ll_set(struct LinkedList *list, int index, void *in);
+int ll_find(struct LinkedList *list, void *item, bool (* comparator)(void*, void*));
+bool ll_contains(struct LinkedList *list, void *item, bool (* comparator)(void*, void*));
+void ll_clear(struct LinkedList *list);
+struct LinkedList* ll_copy(struct LinkedList *list);
+void ll_reverse(struct LinkedList *list);
+void ll_sort(struct LinkedList *list, bool (* comparator)(void*, void*));
+
 
 int ll_size(struct LinkedList *list);
 bool ll_is_empty(struct LinkedList *list);
