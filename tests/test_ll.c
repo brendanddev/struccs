609a19eb23e5a81b21db8d5b5d25001e738881e4
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-11-19 17:49:07 -0500

    Implement func to test get and set

--- tests/test_ll.c
+++ tests/test_ll.c
@@ -22,6 +22,8 @@ int main() {
     ll_reverse(linked_list);
     ll_print(linked_list, print_int);
 
+    test_get_set(linked_list);
+
    
 
 
@@ -34,4 +36,35 @@ int main() {
     ll_discard(linked_list);
     linked_list = NULL;
     return 0;
+}
+
+
+
+void test_get_set(struct LinkedList *list) {
+    int num;
+    if (ll_get(list, 1, &num)) {    // should work
+        printf("Value at index 1: %d\n", num);
+    } else {
+        printf("Could not get the item\n");
+    }
+
+    if (ll_get(list, list->length + 1, &num)) { // should fail
+        printf("Value at index %d: %d\n", list->length + 1, num);
+    } else {
+        printf("Could not get the item\n");
+    }
+
+    
+    int value = 3454;
+    if (ll_set(list, 1, &value)) {      // should work
+        printf("Set the item at index 1 to: %d\n", value);
+    } else {
+        printf("Failed to set the item\n");
+    }
+
+    if (ll_set(list, -1, &value)) {      // should fail
+        printf("Set the item at index 1 to: %d\n", value);
+    } else {
+        printf("Failed to set the item\n");
+    }
 }
