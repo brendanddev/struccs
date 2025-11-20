f3514d8268c18246645b640ed9da16616a52021d
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-11-20 18:27:36 -0500

    Update generic array test suite

---
+++ tests/test_ga.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "test_common.h"
+#include "../generic_array/generic_array.h"
+
+
+/// test_ga.c
+/// The test suite for the GenericArray implementation
+/// Brendan Dileo, November 20 2025
+
+
+
+
+int main() {
+
+    struct GenericArray *generic_array = create_int_ga();
+    
+
+}
