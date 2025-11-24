cf28eee78b51c43e62d81da16890aaea9321c44d
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-11-24 08:46:57 -0500

    Update header and source

--- queue/queue.c
+++ queue/queue.c
@@ -10,3 +10,17 @@
 /// Brendan Dileo - November 24 2025
 
 
+// Defines the Node struct type
+typedef struct Node {
+    void *value;            // Pointer to the value stored in the node
+    size_t item_size;       // The size of the item stored in this node
+    struct Node *next;      // Pointer to the next node in the queue
+} Node;
+
+
+// Prototypes
+static struct Node* queue_create_node(void *val, size_t size);
+static void queue_discard_node(struct Node *node);
+static void queue_discard_all_nodes(struct Queue *queue);
+
+
--- queue/queue.h
+++ queue/queue.h
@@ -12,4 +12,16 @@
 #include <stdbool.h>
 
 
+// Forward declaration of the struct Node stored in the queue
+struct Node;
+
+
+// Defines the Queue struct type
+typedef struct Queue {
+    struct Node *head;          // Pointer to the head node of the queue
+    struct Node *tail;          // Pointer to the tail node of the queue
+    int length;                 // The length of the queue
+} Queue;
+
+
 #endif
