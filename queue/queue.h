8285e9cc1715300c394ebbc568a970f77ecf50f4
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-11-24 08:31:09 -0500

    Create queue header

---
+++ queue/queue.h
@@ -0,0 +1,15 @@
+
+/// stack.h
+/// Header for the generic queue implementation backed by linked nodes
+/// Brendan Dileo - November 24 2025
+
+
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+#endif
