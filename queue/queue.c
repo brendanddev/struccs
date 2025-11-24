f27b7fc377902e2308e3624da6c6878c52199a72
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-11-24 08:31:15 -0500

    Create queue source

---
+++ queue/queue.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include "queue.h"
+
+
+/// queue.c
+/// A generic implementation of a queue backed by linked nodes
+/// Brendan Dileo - November 24 2025
+
+
