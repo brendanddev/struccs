53728d84b47ef76c4e648995636ee85ca1c09bc7
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-11-24 12:25:16 -0500

    Update to test enqueue logic

--- tests/test_queue.c
+++ tests/test_queue.c
@@ -7,3 +7,27 @@
 /// test_queue.c
 /// The test suite for the Queue implementation
 /// Brendan Dileo, November 24 2025
+
+
+int main() {
+
+
+    struct Queue *queue = queue_create();
+    printf("Initial State of Queue: Length: %d, Head: %p, Tail: %p\n", queue->length, queue->head, queue->tail);
+
+    int value = 5000;
+    queue_enqueue(queue, &value, sizeof(int));
+    printf("State of Queue: Length: %d, Head: %p, Tail: %p\n", queue->length, queue->head, queue->tail);
+
+    int value2 = 999;
+    queue_enqueue(queue, &value2, sizeof(int));
+    printf("State of Queue: Length: %d, Head: %p, Tail: %p\n", queue->length, queue->head, queue->tail);
+
+    // void queue_enqueue(struct Queue *queue, void *val, size_t item_size) {
+
+
+
+
+
+    return 0;
+}
