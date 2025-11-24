972f9ac0212aa741688d3ab02424e822316ac50f
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-11-24 17:00:03 -0500

    Create hash table source

---
+++ hash_table/hash_table.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+
+/// hash_table.c
+/// A generic implementation of a hash table
+/// Brendan Dileo - November 24 2025
