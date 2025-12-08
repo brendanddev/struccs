681227e9f05534f2ba9dbc81f8c9bcca80fa6774
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-12-08 12:25:01 -0500

    Create bt test suite

---
+++ tests/test_bt.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "../binary_tree/binary_tree.h"
+
+
+/// test_bt.c
+/// The test suite for the BinaryTree implementation
+/// Brendan Dileo, December 8 2025
+
