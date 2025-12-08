b284e67bfde4bb095ee3fc4ef906075a8e7253c5
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-12-08 12:25:08 -0500

    Setup basic bt test

--- tests/test_bt.c
+++ tests/test_bt.c
@@ -8,3 +8,18 @@
 /// The test suite for the BinaryTree implementation
 /// Brendan Dileo, December 8 2025
 
+
+int main() {
+
+    struct BinaryTree *binarytree = bt_create();
+    printf("Root Node: %p, Length: %d\n", binarytree->root, binarytree->length);
+
+
+
+
+
+
+
+    return 0;
+
+}
