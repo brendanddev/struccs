5a5cb897ac164b2d3d8b1c2cabeb06b16e04741c
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-12-09 15:09:56 -0500

    Setup source for bst

--- binary_search_tree/binarysearch_tree.c
+++ binary_search_tree/binarysearch_tree.c
@@ -1,6 +1,33 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include "binarysearch_tree.h"
 
 
 /// binarysearch_tree.c
 /// A generic implementation of a binary search tree
 /// Brendan Dileo - December 8 2025
+
+
+// Defines the Node struct type
+typedef struct Node {
+
+} Node;
+
+
+// Prototypes
+// ...
+
+
+// Creates a new binary search tree
+struct BinarySearchTree* bst_create() {
+    struct BinarySearchTree *tree = malloc(sizeof(struct BinarySearchTree));
+    return tree;
+
+}
+
+
+
+
+// Private helper functions, linkage limited to this file
