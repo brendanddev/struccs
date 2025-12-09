e7f635c24df75811714392c74570a83dba419c89
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-12-09 12:17:25 -0500

    Rename bst files and start fresh given bugs

---
+++ binary_search_tree/binarysearch_tree.c
@@ -0,0 +1,6 @@
+
+
+
+/// binarysearch_tree.c
+/// A generic implementation of a binary search tree
+/// Brendan Dileo - December 8 2025
---
+++ binary_search_tree/binarysearch_tree.h
@@ -0,0 +1,19 @@
+
+
+/// binarysearch_tree.h
+/// Header for the generic binary search tree implementation with linked nodes
+/// Brendan Dileo - December 8 2025
+
+
+#ifndef BINARYSEARCH_TREE_H
+#define BINARYSEARCH_TREE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+
+
+
+#endif
