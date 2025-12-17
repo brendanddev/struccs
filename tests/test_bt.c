d6a1d95c5c6ece2adcc0e3d8d8c2a53d616d5387
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-12-17 17:53:20 -0500

    Create and update complete binary tree test file

---
+++ tests/test_bt.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "../binary_tree/binary_tree.h"
+
+
+/// test_bt.c
+/// The test suite for the BinaryTree implementation
+/// Brendan Dileo, December 8 2025
+
+
+void print_int(void *value) {
+    printf("%d\n", * (int *) value);
+}
+
+int compare_int(void *a, void *b) {
+    int num1 = * (int *)a;
+    int num2 = * (int *)b;
+
+    if (num1 == num2) {
+        return 0;
+    } else if (num1 > num2) {
+        return 1;
+    } else {
+        return -1;
+    }
+}
+
+
+int main() {
+
+    // Create a new binary tree
+    struct BinaryTree *tree = bt_create(sizeof(int));
+    printf(">>> Created BinaryTree - Length: %d, Capacity: %d\n", tree->length, tree->capacity);
+
+
+
+    return 0;
+}
