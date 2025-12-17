0afa48e29c93590106cd0ccc6d2b2744c806cdec
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-12-17 18:06:34 -0500

    Implement common functions, and parent/left/right child helpers

--- binary_tree/binary_tree.c
+++ binary_tree/binary_tree.c
@@ -10,6 +10,12 @@
 /// Brendan Dileo - December 17 2025
 
 
+// Prototypes
+static int parent_index(int index);
+static int left_child_index(int index);
+static int right_child_index(int index);
+
+
 // Creates a new binary tree storing elements of the provided size
 struct BinaryTree* bt_create(size_t element_size) {
 
@@ -31,4 +37,46 @@ struct BinaryTree* bt_create(size_t element_size) {
     }
 
     return tree;
+}
+
+// Returns the current size (length) of the binary tree
+int bt_size(struct BinaryTree *tree) {
+    return tree->length;
+}
+
+// Returns the current capacity of the tree's underlying array
+int bt_capacity(struct BinaryTree *tree) {
+    return tree->capacity;
+}
+
+// Checks if the binary tree is empty
+bool bt_isempty(struct BinaryTree *tree) {
+    return tree->length == 0;
+}
+
+// Frees the memory previously allocated for the binary tree
+void bt_discard(struct BinaryTree *tree) {
+    if (tree != NULL) {
+        free(tree->elements);
+        free(tree);
+    }
+}
+
+
+// Private helper functions, linkage limited to this file
+
+
+// Returns the array index of the parent of the element at index `index` in the tree
+static int parent_index(int index) {
+    return (index - 1) / 2;
+}
+
+// Returns the array index of the left child of the element at the provided index in the tree
+static int left_child_index(int index) {
+    return 2 * index + 1;
+}
+
+// Returns the array index of the right child of the element at the provided index in the tree
+static int right_child_index(int index) {
+    return 2 * index + 2;
 }
