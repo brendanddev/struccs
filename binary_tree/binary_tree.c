5beebbe2202b45f53d441dbe0ce8a2df07082159
Brendan Dileo <124395674+devDrendos@users.noreply.github.com>
2025-12-17 17:50:55 -0500

    Implement create function for the complete binary tree

--- binary_tree/binary_tree.c
+++ binary_tree/binary_tree.c
@@ -9,3 +9,26 @@
 /// A generic implementation of a complete binary tree
 /// Brendan Dileo - December 17 2025
 
+
+// Creates a new binary tree storing elements of the provided size
+struct BinaryTree* bt_create(size_t element_size) {
+
+    // Allocate memory for the binary tree struct itself and handle allocation failure
+    struct BinaryTree *tree = malloc(sizeof(struct BinaryTree));
+    if (tree == NULL) return NULL;
+
+    // Set initial capacity, length and size of elements stored in the tree
+    tree->capacity = 12;
+    tree->length = 0;
+    tree->element_size = element_size;
+
+    // Allocate memory for the underlying array that will hold the tree elements
+    // and handle allocation failure
+    tree->elements = malloc(tree->capacity * tree->element_size);
+    if (tree->elements == NULL) {
+        free(tree);
+        return NULL;
+    }
+
+    return tree;
+}
